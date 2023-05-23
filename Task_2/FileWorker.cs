22f1491e5b684c7be8899ca43c9fb2eb009a3bf7
Георгиев Дмитрий <dima.georgiev.2016@mail.ru>
2023-05-23 08:14:06 +0300

    Добавьте файлы проекта.

---
+++ Task_2/FileWorker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using TextParserLib;
+
+namespace FileWorkerEXE
+{
+    /// <summary>
+    /// Класс для работы с файлами.
+    /// </summary>
+    public class FileWorker
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Work();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Метод для работы с файлами.
+        /// </summary>
+        private static void Work()
+        {
+            Console.Write("Введите путь к файлу с текстом: ");
+            var inputFilePath = Console.ReadLine();
+
+            string text = File.ReadAllText(inputFilePath).ToLower(); // Читаем весь текст из файла и приводим его к нижнему регистру
+
+            #region Работа с System.Reflection
+
+            // Создаем экземпляр класса Parser из библиотеки TextParserLib
+            var parserInstance = new Parser();
+
+            // Получаем тип внутреннего класса Parser
+            var type = typeof(Parser);
+
+            // Получаем метод Parse с флагом BindingFlags.NonPublic | BindingFlags.Instance
+            var parserMethod = type.GetMethod("Parse", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            // Передаем строку для парсинга в метод Parse через объект parserInstance
+            var result = parserMethod.Invoke(parserInstance, new object[] { text }) as Dictionary<string, int>;
+
+            #endregion
+
+            var resultString = new StringBuilder("");
+            foreach (var value in result)
+            {
+                resultString.Append($"{value.Key}\t{value.Value}\n");
+            }
+
+            Console.Write("Введите путь к файлу, в который будет выведен ответ: ");
+            var outputFilePath = Console.ReadLine();
+
+            using (FileStream fs = File.Create(outputFilePath)) // Записываем результат в файл
+            {
+                Byte[] title = new UTF8Encoding(true).GetBytes(resultString.ToString());
+                fs.Write(title, 0, title.Length);
+            }
+
+            Console.WriteLine("Работа программы завершена...");
+        }
+    }
+}
---
+++ TextPapserLib/Parser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextParserLib
+{
+    /// <summary>
+    /// Класс из dll для парсинга большого текста.
+    /// </summary>
+    public class Parser
+    {
+        /// <summary>
+        /// Метод для парсинга большого текста.
+        /// </summary>
+        /// <param name="text">Передаваемый текст.</param>
+        /// <returns>Словарь с ключами и значениями, где ключ - слово, значение - его кол-во в тексте.</returns>
+        private Dictionary<string, int> Parse(string text)
+        {
+            var wordsDict = new Dictionary<string, int>();
+            
+            Regex regex = new Regex(@"\b(?![×÷])[A-Za-zÀ-ÿа-яА-Я']+\b");
+
+            MatchCollection words = regex.Matches(text);
+            
+            foreach (Match word in words)
+            {
+                if (wordsDict.ContainsKey(word.Value) == false)
+                {
+                    wordsDict.Add(word.Value, 1);
+                }
+                else
+                {
+                    wordsDict[word.Value]++;
+                }
+            }
+
+            var ordered = wordsDict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            return ordered;
+        }
+    }
+}
