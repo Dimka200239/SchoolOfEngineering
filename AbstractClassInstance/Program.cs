e243b05719413a28451a53b498f3ff2c5867e84c
Георгиев Дмитрий <dima.georgiev.2016@mail.ru>
2023-05-23 15:26:52 +0300

    Выполенение второй части первого домашнего задания

---
+++ AbstractClassInstance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AbstractClassInstance
+{
+    public abstract class AbstractClass
+    {
+        private string _name;
+
+        protected AbstractClass(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public abstract string GetName();
+    }
+
+    public class AbstractClassInheritor : AbstractClass
+    {
+        public override string GetName()
+        {
+            return this.Name;
+        }
+
+        public AbstractClassInheritor(string name)
+            : base(name)
+        {
+
+        }
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Первый вариант
+            AbstractClass newAbstractClass = new AbstractClassInheritor("Это точно экземпляр абстрактного класса?");
+
+            Console.WriteLine(newAbstractClass.GetName());
+
+            // Второй вариант с помощью рефлексии
+            Type type = typeof(AbstractClassInheritor);
+
+            AbstractClass abstractClassInstance = (AbstractClass)Activator.CreateInstance(type, "Это точно экземпляр абстрактного класса?");
+
+            Console.WriteLine(abstractClassInstance.GetName());
+        }
+    }
+}
